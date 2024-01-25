75e793e103a11719c52b81e5f39232fa7d0c47a7
cevival <117659719+cevival@users.noreply.github.com>
2024-01-25 15:54:54 +0100

    no message

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,16 +1,13 @@
 import random
-import csv
 
 class Labyrinthe :
     # constructeur
-    def __init__(self, sizeX, sizeY, filesName):
+    def __init__(self, sizeX, sizeY):
         """sizeX, sizeY désignent la taille du labyrinthe sur l'axe (x,y)"""
         self.sizeX = sizeX
         self.sizeY = sizeY
         #attention création d'une matrice en Y X
         self.matrice = [ [0]* self.sizeX for _ in range(self.sizeY) ]
-        self.fn = filesName
-        self.map = []
 
     def affiche(self):
         """Sortie console du labyrinthe"""
@@ -19,17 +16,7 @@ class Labyrinthe :
                 # rappel: matrice en Y,X
                 print(self.matrice[j][i], end = "")
             print()
-
-    def readFiles(self):
-        with open(self.fn, newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            for row in spamreader:
-                rowListe = [int(cell)for cell in row]
-                self.map.append(rowListe)
-    
-    def achiffeMap(self):
-        for row in self.map: 
-            print(row)
+        #print(self.matrice)
 
     def get_matrice(self):
         """renvoie la matrice associée au labyrinthe"""
@@ -51,16 +38,29 @@ class Labyrinthe :
         """Détruit un mur du labyrinthe en (i,j) sur l'axe (x,y)"""
         self.matrice[j][i]=0
 
+    def load_from_file(self, filename):
+        """Charge un labyrinthe d'un fichier texte"""
+        with open(filename) as file:
+            lines = [line.rstrip() for line in file]
+        #print(lines)
+        for i in range(len(lines)):
+            tmp = lines[i]
+            tmp_list = tmp.split(',')
+            for j in range(len(tmp_list)):
+                tmp_list[j]= int(tmp_list[j])
+            print(tmp_list)
+            self.matrice[i]=tmp_list
 
-laby = Labyrinthe(12,5,'laby-01.csv')
-laby.setXY(5,2,1)
-print(laby.getSize())
+
+    def draw(self, screen, tilesize):
+        pass
+
+
+laby = Labyrinthe(20,10)
+laby.load_from_file("laby-01.csv")
 laby.affiche()
 
-print(laby.get_matrice())
-print(laby.getXY(5,2))
-
-
+"""
 l1 = [1, 2, 3, 4, 5]
 l2 = [6, 7, 8, 9, 10]
 l3 = [11,12,13,14,15]
@@ -71,5 +71,4 @@ lst.append(l3)
 print(lst)
 
 print(lst[2][1])
-laby.readFiles()
-laby.achiffeMap()
+"""
